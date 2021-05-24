2a541489e3f4081f54950603ef96ec38fc7e1cd3
deyaa1997 <deyah.mosa@gmail.com>
2021-05-24 16:55:39 +0300

    Pulse Check 5

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import React, {useState} from "react";
 // jsx
 const App = () => {
 
-  const [posts, setPosts] = useState([{userId:2,id:101, title:"", body:""},{userId:3,id:102, title:"", body:""}])
+  const [posts, setPosts] = useState([{userId:2,id:101, title:"dd", body:"dd"},{userId:3,id:102, title:"ff", body:"ff"}])
+  const title = posts.map((elem , i)=>{
+    return (<div><h1>{elem.title}</h1>
+      <p>{elem.body}</p></div> )
+  })
+
+
   return (
     <div>
       <h1>Blog App</h1>
+      {title}
     </div>
   );
 };
