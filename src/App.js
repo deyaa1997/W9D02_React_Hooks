81783e124847cb5d3bd9f71220960bdc5b56a5af
deyaa1997 <deyah.mosa@gmail.com>
2021-05-24 22:58:44 +0300

    Pulse Check 7

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ const App = () => {
 
   const [posts, setPosts] = useState([{userId:2,id:101, title:"dd", body:"dd"},{userId:3,id:102, title:"ff", body:"ff"}])
   const render = posts.map((elem , i)=>{
-    return (<div><h1>{elem.title}</h1>
-      <p>{elem.body}</p></div> )
+    return (<div><h1 key={i}>{elem.title}</h1>
+      <p key={i+10}>{elem.body}</p></div> )
   })
   const [userId,setuserId] = useState(2)
   const [id,setid] = useState(101)
@@ -17,10 +17,22 @@ const App = () => {
     <div>
       <h1>Blog App</h1>
       {render}
-      <input type="number" placeholder="userId"/>
-      <input type="number" placeholder="id"/>
-      <input type="string" placeholder="title"/>
-      <input type="string" placeholder="body"/>
+      <input type="number" placeholder="userId" onChange={(e)=>{
+        setuserId(e.target.value)
+      }
+      }/>
+      <input type="number" placeholder="id" onChange={(e)=>{
+        setid(e.target.value)
+      }
+      }/>
+      <input type="string" placeholder="title" onChange={(e)=>{
+        settitle(e.target.value)
+      }
+      }/>
+      <input type="string" placeholder="body" onChange={(e)=>{
+        setbody(e.target.value)
+      }
+      }/>
       <button>Click To Change</button>
     </div>
   );
