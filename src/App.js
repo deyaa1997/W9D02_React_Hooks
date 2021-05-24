daff77a2e97f5a3ebe2dd054df2f6746089e7a15
deyaa1997 <deyah.mosa@gmail.com>
2021-05-24 16:31:58 +0300

    Pulse Check 4

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 // jsx
 const App = () => {
 
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState([{userId:2,id:101, title:"", body:""},{userId:3,id:102, title:"", body:""}])
   return (
     <div>
       <h1>Blog App</h1>
