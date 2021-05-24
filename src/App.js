64e375822c5034deb2586725391bf61f4160c729
deyaa1997 <deyah.mosa@gmail.com>
2021-05-24 16:28:28 +0300

    Pulse Check3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 
 // jsx
 const App = () => {
+
+  const [posts, setPosts] = useState()
   return (
     <div>
       <h1>Blog App</h1>
