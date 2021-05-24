258c5bae088ec894635655e3388570a7b0142034
deyaa1997 <deyah.mosa@gmail.com>
2021-05-24 22:50:19 +0300

    Pulse Check 6

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import React, {useState} from "react";
 const App = () => {
 
   const [posts, setPosts] = useState([{userId:2,id:101, title:"dd", body:"dd"},{userId:3,id:102, title:"ff", body:"ff"}])
-  const title = posts.map((elem , i)=>{
+  const render = posts.map((elem , i)=>{
     return (<div><h1>{elem.title}</h1>
       <p>{elem.body}</p></div> )
   })
-
+  const [userId,setuserId] = useState(2)
+  const [id,setid] = useState(101)
+  const [title,settitle] = useState("food")
+  const [body,setbody] = useState("broasted")
 
   return (
     <div>
       <h1>Blog App</h1>
-      {title}
+      {render}
+      <input type="number" placeholder="userId"/>
+      <input type="number" placeholder="id"/>
+      <input type="string" placeholder="title"/>
+      <input type="string" placeholder="body"/>
+      <button>Click To Change</button>
     </div>
   );
 };
